58bd5c140f154d23ec950f4face7254ac4923fcf
Ville Andersson <winstoni@windowslive.com>
2021-11-27 20:13:35 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">REACT ROUTER</h1>
+      <BrowserRouter>
+        <Link to="/">Home</Link> <Link to="/About">About</Link>{" "}
+        <Link to="/Contact">Contact</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
